e924451c6f6a2221444b598634081c21a9fec622
Effnkid <l.bobbsemple.gmail.com>
2022-08-19 11:28:57 -0400

    gum 1

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,30 @@
-const router = require('express').Router()
-
+const router = require('express').Router();
+const { Album, Song, Artist } = require('../db');
 // connect your API routes here!
 
-module.exports = router
+router.get('/albums', async (req, res, next) => {
+	try {
+		const all = await Album.findAll({ include: { model: Artist } });
+		res.send(all);
+	} catch (e) {
+		next(e);
+	}
+});
+router.get('/albums/:albumId', async (req, res, next) => {
+	try {
+		const all = await Album.findOne({
+			where: {
+				id: req.params.albumId,
+			},
+			include: {
+				model: Artist,
+				model: Song,
+			},
+		});
+		res.send(all);
+	} catch (e) {
+		next(e);
+	}
+});
+
+module.exports = router;
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,24 @@
-const db = require('./db')
+const db = require('./db');
 // require each of your models here...
-
+const Album = require('./album');
+const Song = require('./song');
+const Artist = require('./artist');
 // ...and give them some nice associations here!
 
+Album.belongsTo(Artist);
+Artist.hasMany(Album);
+
+Song.belongsTo(Artist);
+Artist.hasMany(Song);
+
+Song.belongsTo(Album);
+Album.hasMany(Song);
+
 module.exports = {
-  db,
-  // Include your models in your module.exports as well!
-  // The seed file expects to find them there!
-}
+	db,
+	Album,
+	Song,
+	Artist,
+	// Include your models in your module.exports as well!
+	// The seed file expects to find them there!
+};
