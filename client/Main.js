a941cab421f2d3ed3e52acb88ac1f83fd16243cf
Effnkid <l.bobbsemple.gmail.com>
2022-08-19 12:11:10 -0400

    gum 3

--- client/Main.js
+++ client/Main.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Sidebar from './conponents/Sidebar';
 import Container from './conponents/Container';
 import Player from './conponents/Player';
+import Axios from 'axios';
 
 const Main = () => {
+	const [albumList, setAlbumList] = React.useState([]);
+
+	React.useEffect(() => {
+		const data = async () => {
+			const res = await Axios.get('/api/albums');
+			setAlbumList(res.data);
+		};
+		data();
+	}, []);
+
 	return (
 		<div id="main" className="row container">
 			<Sidebar />
-			<Container />
+			<Container albumList={albumList} />
 			<Player />
 		</div>
 	);
